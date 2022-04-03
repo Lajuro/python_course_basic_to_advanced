3fe8eb69b0d794be8383aae852c9ad36762e38c8
Roberto Camargo <robertoc96@outlook.com>
2022-04-03 11:57:17 -0300

    Added details about Django Admin, how to create models to be displayed at the Django Admin page.

--- 06_django/02_project_schedule/agenda/contatos/admin.py
+++ 06_django/02_project_schedule/agenda/contatos/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Contato, Categoria
 
-# Register your models here.
+
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'sobrenome', 'email', 'telefone', 'data_criacao', 'categoria')
+    list_display_links = ('id', 'nome', 'sobrenome')
+    list_filter = ('categoria',)
+    search_fields = ('nome', 'email', 'telefone')
+    ordering = ('id',)
+    list_per_page = 5
+
+
+admin.site.register(Categoria)
+admin.site.register(Contato, ContatoAdmin)
--- 06_django/02_project_schedule/agenda/contatos/models.py
+++ 06_django/02_project_schedule/agenda/contatos/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Categoria(models.Model):
     nome = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.nome
+
 
 class Contato(models.Model):
     nome = models.CharField(max_length=255)
@@ -13,5 +16,8 @@ class Contato(models.Model):
     data_criacao = models.DateTimeField(auto_now_add=True)
     descricao = models.TextField(blank=True, null=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return f'{self.nome} {self.sobrenome}'
 
 
